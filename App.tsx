d50031f98ca9d179ed486060b9f7f7735e9becb3
Swanand Vaidya <ssvaidya28@gmail.com>
2025-01-13 23:44:27 +0530

    renamed text name

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const App = () => {
   return (
     <SafeAreaView>
-      <Text style={{fontSize: 24, textAlign: 'center'}}>Rn UI components</Text>
+      <Text style={{fontSize: 24, textAlign: 'center'}}>UI Components</Text>
     </SafeAreaView>
   );
 };
